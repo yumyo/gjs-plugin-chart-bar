b44ae81368ad0ac65c820b9f02bbc19611be657f
kylerc19 <km@raconteur.net>
2020-01-10 15:30:33 +0000

    typo fixes

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 export const
-  hChartDonutRef = 'h-zing-bar',
-  chartDonutRef = 'zing-bar',
+  hChartBarRef = 'h-zing-bar',
+  chartBarRef = 'zing-bar',
   black = '#000000',
   white = '#ffffff',
   navy = '#002a5e',
